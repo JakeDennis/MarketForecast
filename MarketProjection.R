3a7875ac2abc42f42b8b2ff646c1e8de4ab0ec98
Jake Dennis <jakejdennis@gmail.com>
2020-11-10 10:13:03 -0600

    Add initial regression model

--- MarketProjection.R
+++ MarketProjection.R
@@ -50,6 +50,13 @@ df$log_pct_change = diff(df$log_price)/df$log_price * 100
 df$log_pct_change = data.table::shift(df$log_pct_change, n=1, fill=NA, type="lag")
 df$vix_change = diff(df$vix)/df$vix * 100
 df$vix_change = data.table::shift(df$vix_change, n=1, fill=NA, type="lag")
+
+spx_pct = ggplot(df, aes(date, pct_change)) +
+  geom_line(na.rm = TRUE) + 
+  ggtitle("Percent Change of SPX from Prior Close")+
+  xlab("Date") + ylab("Percent Change")
+spx_pct
+
 ggAcf(df$pct_change)
 ggAcf(df$log_pct_change)
 ggAcf(diff(df$vix))
@@ -175,10 +182,119 @@ ggplot(df, aes(x=daysUntilOpEx, y=vix_change, group=daysUntilOpEx)) +
   labs(title = "Days until Options Expiration Effect on VIX")+
   xlab("Days until OpEx Date") + ylab("VIX Percent Change")
 
-#create moving averages
+#create time series for modeling 
+projts<-ts(data.frame(df$vix_change,df$dix,df$gex,df$daysUntilOpEx,df$Monday,df$Tuesday,df$Wednesday,df$Thursday,df$Friday), names = c("VIX_CHANGE","DIX","GEX","daysUntilOpEx","MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY" ), frequency = 1)
+head(projts)
+#create target time series
+VIXts<-ts(projts[29:2382,"VIX_CHANGE"], names = c("VIX_chg"))
+head(VIXts)
+#create lags of GEX and DIX for modeling
+GEXLag <- ts(cbind(
+  GEXLag1 = lag(projts[,"GEX"],-1),
+  GEXLag2 = lag(projts[,"GEX"],-2),
+  GEXLag3 = lag(projts[,"GEX"],-3),
+  GEXLag4 = lag(projts[,"GEX"],-4),  
+  GEXLag5 = lag(projts[,"GEX"],-5),
+  GEXLag6 = lag(projts[,"GEX"],-6),
+  GEXLag7 = lag(projts[,"GEX"],-7),
+  GEXLag8 = lag(projts[,"GEX"],-8),
+  GEXLag9 = lag(projts[,"GEX"],-9),
+  GEXLag10 = lag(projts[,"GEX"],-10),  
+  GEXLag11 = lag(projts[,"GEX"],-11),
+  GEXLag12 = lag(projts[,"GEX"],-12),
+  GEXLag13 = lag(projts[,"GEX"],-13),
+  GEXLag14 = lag(projts[,"GEX"],-14),
+  GEXLag15 = lag(projts[,"GEX"],-15),
+  GEXLag16 = lag(projts[,"GEX"],-16),
+  GEXLag17 = lag(projts[,"GEX"],-17),  
+  GEXLag18 = lag(projts[,"GEX"],-18),
+  GEXLag19 = lag(projts[,"GEX"],-19),
+  GEXLag20 = lag(projts[,"GEX"],-20),
+  GEXLag21 = lag(projts[,"GEX"],-21),
+  GEXLag22 = lag(projts[,"GEX"],-22),
+  GEXLag23 = lag(projts[,"GEX"],-23),  
+  GEXLag24 = lag(projts[,"GEX"],-24),
+  GEXLag25 = lag(projts[,"GEX"],-25),
+  GEXLag26 = lag(projts[,"GEX"],-26),
+  GEXLag27 = lag(projts[,"GEX"],-27),
+  GEXLag28 = lag(projts[,"GEX"],-28)))
+head(GEXLag)
+GEXlagts <- GEXLag[29:2382,]
+DIXLag <- cbind(
+  DIXLag1 = lag(projts[,"DIX"],-1),
+  DIXLag2 = lag(projts[,"DIX"],-2),
+  DIXLag3 = lag(projts[,"DIX"],-3),
+  DIXLag4 = lag(projts[,"DIX"],-4),  
+  DIXLag5 = lag(projts[,"DIX"],-5),
+  DIXLag6 = lag(projts[,"DIX"],-6),
+  DIXLag7 = lag(projts[,"DIX"],-7),
+  DIXLag8 = lag(projts[,"DIX"],-8),
+  DIXLag9 = lag(projts[,"DIX"],-9),
+  DIXLag10 = lag(projts[,"DIX"],-10),  
+  DIXLag11 = lag(projts[,"DIX"],-11),
+  DIXLag12 = lag(projts[,"DIX"],-12),
+  DIXLag13 = lag(projts[,"DIX"],-13),
+  DIXLag14 = lag(projts[,"DIX"],-14),
+  DIXLag15 = lag(projts[,"DIX"],-15),
+  DIXLag16 = lag(projts[,"DIX"],-16),
+  DIXLag17 = lag(projts[,"DIX"],-17),  
+  DIXLag18 = lag(projts[,"DIX"],-18),
+  DIXLag19 = lag(projts[,"DIX"],-19),
+  DIXLag20 = lag(projts[,"DIX"],-20),
+  DIXLag21 = lag(projts[,"DIX"],-21),
+  DIXLag22 = lag(projts[,"DIX"],-22),
+  DIXLag23 = lag(projts[,"DIX"],-23),  
+  DIXLag24 = lag(projts[,"DIX"],-24),
+  DIXLag25 = lag(projts[,"DIX"],-25),
+  DIXLag26 = lag(projts[,"DIX"],-26),
+  DIXLag27 = lag(projts[,"DIX"],-27),
+  DIXLag28 = lag(projts[,"DIX"],-28))
+head(DIXLag)
+DIXlagts <- DIXLag[29:2382,]
+head(DIXlagts)
+#create other variables ts
+Timets <- ts(cbind(
+  OpExts = projts[29:2382,"daysUntilOpEx"],
+  Mondayts = projts[29:2382,"MONDAY"],
+  Tuesdayts = projts[29:2382,"TUESDAY"],
+  Wednesdayts = projts[29:2382,"WEDNESDAY"],
+  Thursdayts = projts[29:2382,"THURSDAY"],
+  Fridayts = projts[29:2382,"FRIDAY"]))
+head(Timets)
 
-
-
-#find appropriate lag
-
-
+#model with dix and gex lags
+dixgexcolnames<-colnames(dixgex)
+dixgexcolnames
+dixgex<-cbind(VIXts, GEXlagts, DIXlagts)
+head(dixgex)
+dgfit<-lm(VIXts~., data=dixgex)
+summary(dgfit)
+dgfit2<-lm(VIXts~GEXlagts.GEXLag1+GEXlagts.GEXLag2+GEXlagts.GEXLag3+GEXlagts.GEXLag6+GEXlagts.GEXLag12+
+           GEXlagts.GEXLag14+GEXlagts.GEXLag16+GEXlagts.GEXLag22+DIXlagts.DIXLag1+DIXlagts.DIXLag2+
+           DIXlagts.DIXLag3+DIXlagts.DIXLag7+DIXlagts.DIXLag10+DIXlagts.DIXLag19+DIXlagts.DIXLag20, data = dixgex)
+summary(dgfit2)
+dgfit3<-lm(VIXts~GEXlagts.GEXLag1+GEXlagts.GEXLag2+GEXlagts.GEXLag3+GEXlagts.GEXLag6+GEXlagts.GEXLag12+
+             GEXlagts.GEXLag14+GEXlagts.GEXLag16+DIXlagts.DIXLag1+DIXlagts.DIXLag2+
+             DIXlagts.DIXLag19+DIXlagts.DIXLag20, data = dixgex)
+summary(dgfit3)
+dgfit4<-lm(VIXts~GEXlagts.GEXLag1+GEXlagts.GEXLag2+GEXlagts.GEXLag3+GEXlagts.GEXLag6+GEXlagts.GEXLag12+
+             GEXlagts.GEXLag14+DIXlagts.DIXLag1+DIXlagts.DIXLag2+
+             DIXlagts.DIXLag20, data = dixgex)
+summary(dgfit4)
+dgfit5<-lm(VIXts~GEXlagts.GEXLag1+GEXlagts.GEXLag2+GEXlagts.GEXLag3+
+             GEXlagts.GEXLag14+DIXlagts.DIXLag1+DIXlagts.DIXLag2+
+             DIXlagts.DIXLag20, data = dixgex)
+summary(dgfit5)
+dgfit5<-lm(VIXts~GEXlagts.GEXLag1+GEXlagts.GEXLag2+GEXlagts.GEXLag3+
+             GEXlagts.GEXLag14+DIXlagts.DIXLag1+DIXlagts.DIXLag2+
+             DIXlagts.DIXLag20, data = dixgex)
+summary(dgfit5)
+dgfit6<-lm(VIXts~GEXlagts.GEXLag1+GEXlagts.GEXLag2+GEXlagts.GEXLag3+
+             GEXlagts.GEXLag14+DIXlagts.DIXLag1+DIXlagts.DIXLag2, data = dixgex)
+summary(dgfit6)
+#arima model on dixgex
+regex<-cbind(GEXlagts, DIXlagts)
+head(regex)
+dgfitaa<-auto.arima(VIXts, xreg = regex)
+summary(dgfitaa)
+checkresiduals(dgfitaa)
